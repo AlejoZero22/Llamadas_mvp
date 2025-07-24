a3daff07ce931f693735a8e11f785f3f1208c1f3
AlejoZero22 <alejo.apolo36@gmail.com>
2025-07-23 22:51:30 -0500

    Taller directorio (arreglado)
    Se arreglo lo que ya tenia hecho ya que no queria funcionar bien con el QR y tenia otros erres para cargarlo en git pero YA ETA SOLUCIONANDO

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Directorio para tus personajes de JUEGOS</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+return (
+  <view style={styles.container}>
+    <text style={styles.title}>{appTitle}</text>
+    <tex style={styles.title}>Zero - 0204082480</tex>
+    <tex style={styles.title}>Link - 6589478526</tex>
+    <tex style={styles.title}>Don Celular muerta - 6453597850</tex>
+    <tex style={styles.title}>Mega X - 2648975605 </tex>
+    <tex style={styles.title}>Mario - 3689745821</tex>
+
+
+  </view>
+);
